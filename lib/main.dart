c03a3e7b5cfb8808fa5aaa955c5dd69689d15344
unknown <shiahir.bhandariii@gmail.com>
2021-09-23 18:29:28 +0545

    second  commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ void main() {
 }
 
 class Myapp extends StatelessWidget {
-  const Myapp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
